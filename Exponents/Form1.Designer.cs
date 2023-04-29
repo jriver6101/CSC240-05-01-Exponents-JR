ff167cd4394474a2332c9d46cdd1f48091ffc059
Johan Rivera <jriver6101@my.sullivan.edu>
2023-04-28 21:59:25 -0400

    001-coding the buttons

--- Exponents/Form1.Designer.cs
+++ Exponents/Form1.Designer.cs
@@ -37,34 +37,38 @@
             // 
             // xInputTextBox
             // 
-            this.xInputTextBox.Location = new System.Drawing.Point(92, 45);
+            this.xInputTextBox.Location = new System.Drawing.Point(123, 55);
+            this.xInputTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.xInputTextBox.Name = "xInputTextBox";
-            this.xInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xInputTextBox.Size = new System.Drawing.Size(132, 22);
             this.xInputTextBox.TabIndex = 0;
+            this.xInputTextBox.TextChanged += new System.EventHandler(this.xInputTextBox_TextChanged);
             // 
             // xInputLabel
             // 
             this.xInputLabel.AutoSize = true;
-            this.xInputLabel.Location = new System.Drawing.Point(92, 27);
+            this.xInputLabel.Location = new System.Drawing.Point(123, 33);
+            this.xInputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.xInputLabel.Name = "xInputLabel";
-            this.xInputLabel.Size = new System.Drawing.Size(85, 13);
+            this.xInputLabel.Size = new System.Drawing.Size(103, 16);
             this.xInputLabel.TabIndex = 3;
             this.xInputLabel.Text = "Enter an integer:";
             // 
             // xOutputLabel
             // 
             this.xOutputLabel.AutoSize = true;
-            this.xOutputLabel.Location = new System.Drawing.Point(102, 181);
+            this.xOutputLabel.Location = new System.Drawing.Point(137, 228);
+            this.xOutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.xOutputLabel.Name = "xOutputLabel";
-            this.xOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.xOutputLabel.Size = new System.Drawing.Size(0, 16);
             this.xOutputLabel.TabIndex = 4;
-            this.xOutputLabel.Visible = false;
             // 
             // xGoButton
             // 
-            this.xGoButton.Location = new System.Drawing.Point(105, 96);
+            this.xGoButton.Location = new System.Drawing.Point(140, 118);
+            this.xGoButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.xGoButton.Name = "xGoButton";
-            this.xGoButton.Size = new System.Drawing.Size(75, 67);
+            this.xGoButton.Size = new System.Drawing.Size(100, 82);
             this.xGoButton.TabIndex = 6;
             this.xGoButton.Text = "Go!";
             this.xGoButton.UseVisualStyleBackColor = true;
@@ -72,9 +76,10 @@
             // 
             // xResetButton
             // 
-            this.xResetButton.Location = new System.Drawing.Point(105, 212);
+            this.xResetButton.Location = new System.Drawing.Point(140, 261);
+            this.xResetButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.xResetButton.Name = "xResetButton";
-            this.xResetButton.Size = new System.Drawing.Size(75, 23);
+            this.xResetButton.Size = new System.Drawing.Size(100, 28);
             this.xResetButton.TabIndex = 7;
             this.xResetButton.Text = "Reset";
             this.xResetButton.UseVisualStyleBackColor = true;
@@ -82,16 +87,18 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(432, 402);
             this.Controls.Add(this.xResetButton);
             this.Controls.Add(this.xGoButton);
             this.Controls.Add(this.xOutputLabel);
             this.Controls.Add(this.xInputLabel);
             this.Controls.Add(this.xInputTextBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Exponents Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
